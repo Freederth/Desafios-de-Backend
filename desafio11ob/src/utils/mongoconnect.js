c09444caa7b95fe2e4ec71734dc59fcdccd1304f
isabelAchurra <iachurra@udec.cl>
2022-10-08 01:18:17 -0300

    mongo me da problemas

--- desafio11ob/src/utils/mongoconnect.js
+++ desafio11ob/src/utils/mongoconnect.js
@@ -1,10 +1,9 @@
-require("dotenv").config({ path: "../../.env" });
 const mongoose = require("mongoose");
 
 const mongoConnect = async () => {
 	try {
-		const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}`;
-		// mongodb+srv://admin:<password>@cluster0.z9jlepu.mongodb.net/test
+		const url =
+			"mongodb+srv://admin:chmod777@cluster0.z9jlepu.mongodb.net/?retryWrites=true&w=majority";
 		mongoose.connect(url, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
