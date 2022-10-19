9d19f202976a76309a861f0c0e8747654668b1e6
isabelAchurra <iachurra@udec.cl>
2022-10-19 20:16:07 -0300

    quité cosas

--- desafio11ob/server.js
+++ desafio11ob/server.js
@@ -307,32 +307,6 @@ io.on("connection", async socket => {
 });
 // ---------------------------- FIN CARRITO -------------
 
-// ----- INFO PAGE ----
-app.get("/info", (req, res) => {
-	const arguments = argv.slice(2).join(" || ");
-
-	res.render("info", {
-		execArgv: arguments.length ? arguments : "Ninguno",
-		platform,
-		version,
-		memoryUsage: memoryUsage().rss,
-		cwd: cwd(),
-		pid,
-		execPath
-	});
-});
-
-// ----- Random PAGE ----
-app.get("/api/randoms", (req, res) => {
-	let { cant } = req.query;
-	console.log(cant);
-	const random = fork("./src/utils/calculo", [cant]);
-	random.send("start");
-	random.on("message", obj => {
-		res.json(obj);
-	});
-});
-
 //--------- listener
 httpServer.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
